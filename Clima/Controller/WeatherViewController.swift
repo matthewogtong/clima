be10ef0556dafdc30d630d3ba56ec7a913eab67d
matthewogtong <ogtongm@gmail.com>
2021-12-28 19:55:53 -0500

    add textFieldDidEndEditing

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -32,5 +32,9 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+    
 }
 
