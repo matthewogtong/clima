61e016d0a02b292ca81c1298c977b3fd91e12461
matthewogtong <ogtongm@gmail.com>
2021-12-29 21:14:57 -0500

    Implement getConditionName method

--- Clima/WeatherData.swift
+++ Clima/WeatherData.swift
@@ -12,11 +12,18 @@ struct WeatherData: Decodable {
     
     let name: String
     let main: Main
+    let weather: [Weather]
     
 }
 
 struct Main: Decodable {
     
     let temp: Double
+    
+}
+
+struct Weather: Decodable {
+    
+    let id: Int
     
 }
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -52,10 +52,34 @@ struct WeatherManager {
         
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.main.temp)
+            let id = decodedData.weather[0].id
+            print(getConditionName(weatherId: id))
         } catch {
             print(error)
         }
+    }
+    
+    func getConditionName(weatherId: Int) -> String {
+        
+        switch weatherId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+        
     }
     
 }
