ce2a534824411de5d03ea067ff0a472bbac27fa4
matthewogtong <ogtongm@gmail.com>
2021-12-29 20:43:51 -0500

    implement parseJSON method and update performRequest method

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -48,7 +48,14 @@ struct WeatherManager {
     
     func parseJSON(weatherData: Data) {
         
+        let decoder = JSONDecoder()
         
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData)
+        } catch {
+            print(error)
+        }
     }
     
 }
