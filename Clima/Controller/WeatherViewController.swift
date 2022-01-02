b02d19bd3f1dc620462b6a196556c352fb736d7e
matthewogtong <ogtongm@gmail.com>
2022-01-02 11:58:57 -0500

    Implement didUpdateWeather method in WeatherViewController

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -58,6 +58,8 @@ class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManag
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
         DispatchQueue.main.async {
             self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+            self.cityLabel.text = weather.cityName
         }
     }
     
